a539613af709f1a5c0ebb212c6083201cc277e3f
Gavrie Philipson <gavrie@philipson.co.il>
2020-07-20 10:13:46 +0300

    Bring closer to heartbeatd code

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 #![warn(rust_2018_idioms)]
 
 use async_std::io;
+use async_std::net::{Shutdown, TcpListener, TcpStream};
+use async_std::prelude::*;
 use async_std::task;
-use async_std::net::{TcpListener, TcpStream, Shutdown};
 
-use futures::future::try_join;
-use futures::{FutureExt, AsyncWriteExt};
+// use futures::future::try_join;
+// use futures::{AsyncWriteExt, FutureExt};
 use std::env;
 use std::error::Error;
 
@@ -24,11 +25,13 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let listener = TcpListener::bind(listen_addr).await?;
 
     while let Ok((inbound, _)) = listener.accept().await {
-        let transfer = transfer(inbound, server_addr.clone()).map(|r| {
-            if let Err(e) = r {
+        let server_addr = server_addr.clone();
+        let transfer = async move {
+            let res = transfer(inbound, server_addr).await;
+            if let Err(e) = res {
                 println!("Failed to transfer; error={}", e);
             }
-        });
+        };
 
         task::spawn(transfer);
     }
@@ -37,8 +40,52 @@ async fn main() -> Result<(), Box<dyn Error>> {
 }
 
 async fn transfer(inbound: TcpStream, proxy_addr: String) -> Result<(), Box<dyn Error>> {
-    let outbound = TcpStream::connect(proxy_addr).await?;
+    let stop_source = stop_token::StopSource::new();
+    let stop_token = stop_source.stop_token();
 
+    let outbound = TcpStream::connect(proxy_addr);
+    let outbound = stop_token.stop_future(outbound);
+
+    let outbound = match outbound.await {
+        Some(Ok(stream)) => Ok(stream),
+        Some(Err(err)) => {
+            println!(
+                "Failed creating port forwarder to remote connection: {:?}",
+                err
+            );
+            Err(err)
+        }
+        None => {
+            println!("Cancelled while connecting, returning");
+            return Ok(());
+        }
+    }?;
+
+    let (ri, wi) = &mut (&inbound, &inbound);
+    let (ro, wo) = &mut (&outbound, &outbound);
+
+    let client_to_server = async {
+        let res = io::copy(ri, wo).await;
+        println!("Completed client->server copy with {:?}", res);
+        res
+    };
+
+    let server_to_client = async {
+        let res = io::copy(ro, wi).await;
+        println!("Completed server->client copy with {:?}", res);
+        res
+    };
+
+    let copiers = client_to_server.try_join(server_to_client);
+    let copiers = stop_token.stop_future(copiers);
+
+    match copiers.await {
+        Some(Ok(_)) => println!("Forwarding task completed"),
+        Some(Err(e)) => println!("Error during forwarding task: {:?}", e),
+        None => println!("Forwarding task cancelled"),
+    }
+
+    /*
     let (ri, wi) = (inbound.clone(), inbound);
     let (ro, wo) = (outbound.clone(), outbound);
 
@@ -53,6 +100,7 @@ async fn transfer(inbound: TcpStream, proxy_addr: String) -> Result<(), Box<dyn 
     };
 
     try_join(client_to_server, server_to_client).await?;
+    */
 
     Ok(())
 }
